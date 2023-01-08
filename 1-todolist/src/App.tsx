8857efc05aeec4b781e755e32e550e96ff852f0e
dasha7879 <belka7879@gmail.com>
2023-01-08 22:32:34 +0300

    add components

--- 1-todolist/src/App.tsx
+++ 1-todolist/src/App.tsx
@@ -1,32 +1,21 @@
 import React from 'react';
-import { AddTaskForm } from './AddTaskForm';
 import './App.css';
-import TodoList, { TaskType } from "./TodoList";
-import { TodoListHeader } from './TodoListHeader';
-
+import { TaskType, TodoList } from './TodoList';
 
 function App() {
-    const todoListTitle_1: string = "What to learn"
-    const todoListTitle_2: string = "What to buy"
+    const todoListTitle_1:string = "What to learn"
+    const todoListTitle_2:string= "What to buy"
     const tasks_1: Array<TaskType> = [
-        { id: 1, title: "HTML", isDone: true },
-        { id: 2, title: "CSS", isDone: true },
-        { id: 3, title: "JS/TS", isDone: false },
-        { id: 1, title: "HTML", isDone: true },
-        { id: 2, title: "CSS", isDone: true },
-        { id: 3, title: "JS/TS", isDone: false }
-    ]
-    const tasks_2: Array<TaskType> = [
-        { id: 1, title: "Cheese", isDone: true },
-        { id: 2, title: "Milk", isDone: true },
-        { id: 3, title: "Carrot", isDone: false },
-        { id: 1, title: "Onion", isDone: true }
+        {id:1, title:"html", isDone:true},
+        {id:2, title:"css", isDone:true},
+        {id:3, title:"js", isDone:false}
     ]
 
     return (
-        <>
-           <TodoList  title={todoListTitle_1} tasks={tasks_1}/>
-        </>
+        <div className="App">
+            <TodoList title = {todoListTitle_1} tasks = {tasks_1}/>
+            {/* <TodoList title = {todoListTitle_2} tasks = {tasks_1} /> */}
+        </div>
     );
 }
 
--- 1-todolist/src/TodoList.tsx
+++ 1-todolist/src/TodoList.tsx
@@ -1,12 +1,10 @@
-import React, { FC } from 'react';
-import { AddTaskForm } from './AddTaskForm';
-import { TasksList } from './TasksList';
-import { TodoListHeader } from './TodoListHeader';
+import React from 'react';
 
 type TodoListPropsType = {
     title: string
     tasks: Array<TaskType>
 }
+
 export type TaskType = {
     id: number
     title: string
@@ -14,29 +12,39 @@ export type TaskType = {
 
 }
 
-const TodoList: FC<TodoListPropsType> = (props) => {
-    // let tasksList;
-    // if(props.tasks.length === 0){
-    //     tasksList = <span> Your tasklist is empty</span>
-    // }else{
-    //     tasksList = props.tasks.map((task:TaskType) => {
-    //         return(
-    //             <li>
-    //                     <input type="checkbox" checked={task.isDone}/>
-    //                     <span>{task.title}</span>
-    //             </li>
-    //         )
-    //     })
-    // }
+export const TodoList = (props: TodoListPropsType) => {
+
+
+
+    let tasksList = props.tasks.length ?
+        props.tasks.map((task: TaskType) => {
+            return (
+                <li>
+                    <input type="checkbox" checked={task.isDone} />
+                    <span>{task.title}</span>
+                </li>
+            )
+        })
+        : <span>Your tasklistis empty</span>
 
 
     return (
-        <div>
-          <TodoListHeader  title='What to learn'/>
-          <AddTaskForm />
-          <TasksList tasks={props.tasks} />
-        </div>
+        <>
+            <div>
+                <h3>{props.title}</h3>
+                <div>
+                    <input />
+                    <button>+</button>
+                </div>
+                <ul>
+                    {tasksList}
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </>
     )
 }
-
-export default TodoList;
